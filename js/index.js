c9659a0386994be66f0f732737192be23018eb7c
Mariya Kien <kienmariya@gmail.com>
2019-01-18 22:08:40 +0700

    update

--- js/index.js
+++ js/index.js
@@ -2,12 +2,18 @@ $(document).ready(function () {
     const show_mobile_version = () => {
         logo_mobile();
         menu_mobile();
+        $('#hyphen').html('-');
+        $('#hyphen').css('display', "inline");
         main_content_mobile();
+        description_mobile();
     }
     const show_desktop_version = () => {
         logo_desktop();
         menu_desktop();
+        $('#hyphen').html('—');
+        $('span#hyphen').css('display', "inline");
         main_content_desktop();
+        description_desktop();
     }
     const logo_mobile = () => {
         $('#phone-number').insertBefore($('#email'));
@@ -17,6 +23,7 @@ $(document).ready(function () {
         $('#menu').css('display', 'none');
         $('#catalog-menu').css('display', 'none');
         $('#catalog-mobile').append($('#catalog-menu'));
+        $("#menu>li:nth-child(6)").insertBefore($("#menu>li:nth-child(5)"));
         replace_in_catalog_menu_mobile();
     }
     const replace_in_catalog_menu_mobile = () => {
@@ -27,6 +34,15 @@ $(document).ready(function () {
     }
     const main_content_mobile = () => {
         $('#content-items').after($('#side-menu'));
+        console.log( $("#news-menu>li:nth-child(1) a"));
+        $("#news-menu>li:nth-child(1) a").html('Поздравительная речь президента международной корпорации Хуа Шэн господина Ли Вея в Международн…');
+        $("#news-menu>li:nth-child(5) a").html('Сотрудники иркутского филиала отметили китайский новый');
+    }
+    const description_mobile = () => {
+        $('#data1').html($('#data1').html() + $('#data2').html());
+        $('#data1').addClass('paragraph');
+        $('#data1').css('display', "block");
+        $('#data2').empty();
     }
     const logo_desktop = () => {
         $('#email').insertBefore($('#phone-number'));
@@ -35,6 +51,7 @@ $(document).ready(function () {
     }
     menu_desktop = () => {
         $('#menu').css('display', 'flex');
+        $("#menu>li:nth-child(6)").insertBefore($("#menu>li:nth-child(5)"));
         replace_in_catalog_desktop();
     }
     const replace_in_catalog_desktop = () => {
@@ -47,6 +64,15 @@ $(document).ready(function () {
         $('#catalog-menu').css('display', 'block');
         $('#main-content').append($('#side-menu'));
         $('#side-menu').insertBefore($('#content'));
+        $("#news-menu>li:nth-child(1) a").html('Поздравительная речь президента международной корпорации Хуа Шэн господина Ли Вея в Международный…');
+        $("#news-menu>li:nth-child(5) a").html('Сотрудники иркутского филиала отметили китайский новый год');
+    }
+    const description_desktop = () => {
+        $('#data1').removeClass('paragraph');
+        $('#data1').html('Японские ножи - это прекрасный выбор, который говорит о требовательности покупателя к высокому качеству ножа и о его превосходном вкусе. Кстати, нужно отметить, что японские ножи предназначены не только для японской, но и для европейской, а также любой другой кухни. В известных ресторанах крупнейших городов во всем мире используют именно японские ножи. Японские ножи -это профессиональные инструменты для японской кухни (купить японские ножи Вы можете у нас).');
+        $('#data2').html('Интернет магазин "Chef" предлагает купить японские ножи (ножи касуми, масахиро), нож для суши. У нас есть японские ножи из дамасской стали (ножи masahiro, касуми). ');
+        $('#data1').css('display', "inline");
+        $('#data2').css('display', "inline");
     }
     $('#menu-button').on('click', () => {
         $('#menu').toggle('slow');
@@ -62,6 +88,9 @@ $(document).ready(function () {
     var win_w = $(window).width();
     if (win_w <= 480) {
         show_mobile_version();
+    }
+    if (win_w > 480) {
+        show_desktop_version();
     }
 
     $(window).resize(function () {
